5c63085f4daa2b6df4bd15fa572c0191e2e84bc9
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-09-10 11:58:56 -0600

    submission for deadline

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def favorite_product
+        products.order(:star_rating).last
+    end
+
+    def remove_reviews(product)
+        reviews = self.reviews.find_by_product_id(product.id)
+        if reviews 
+            reviews.destroy
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,46 @@ product4 = Product.create(name: "Ballpoint Pens", price: 2)
 product5 = Product.create(name: "Scotch Tape", price: 3)
 
 puts "Creating reviews..."
-# *****************************************************************
-# * TODO: create reviews! Remember, a review belongs to a product *
-# * and a review belongs to an user.                              *
-# *****************************************************************
-# Create Reviews Here
+review1 = Review.create(star_rating: 4, comment: "Why would you say something like this?", product_id: product1.id, user_id: user2.id)
+review2 = Review.create(star_rating: 3, comment: "This is commodity is insatiable. Wow.", product_id: product2.id, user_id: user3.id)
+review3 = Review.create(star_rating: 4, comment: "You're*", product_id: product3.id, user_id: user1.id)
+review4 = Review.create(star_rating: 1, comment: "First comment", product_id: product1.id, user_id: user3.id)
+review5 = Review.create(star_rating: 2, comment: "You did a bad job, user1", product_id: product5.id, user_id: user1.id)
 
+puts
+puts "User and Product Instances for review1:"
+puts review1.user.name
+puts review1.product.name
+puts
+puts "All reviews for #{product1.name}s:"
+puts product1.reviews.map(&:comment)
+puts
+puts "All users who reviewed #{product1.name}s:"
+puts product1.users.map(&:name)
+puts
+puts "All reviews for #{user3.name}:"
+puts user3.reviews.map(&:comment)
+puts
+puts "All products reviewed by #{user3.name}:"
+puts user3.products.map(&:name)
+puts
+puts "Printing review..."
+review5.print_review
+puts
+puts "Leaving review..."
+new_review = product4.leave_review(user2, 3, "What a shameful little device")
+puts new_review.comment
+puts
+puts "Printing all reviews for #{product1.name}s:"
+puts product1.print_all_reviews
+puts
+# puts "Avg star rating for product1:"
+# puts product1.average_rating
+puts
+puts "#{user1.name}'s Favorite Product:"
+puts user1.favorite_product.name
+puts
+puts "Removing reviews for #{product5.name}s:"
+puts user1.remove_reviews(product5)
+puts
 puts "Seeding done!"
